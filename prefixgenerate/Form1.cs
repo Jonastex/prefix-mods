ed4a81ad37f32de5b013ff68cc0a60e9dd239777
Jonastex <30275981+Jonastex@users.noreply.github.com>
2022-12-19 02:19:40 +0100

    Add files via upload

--- prefixgenerate/Form1.cs
+++ prefixgenerate/Form1.cs
@@ -21,30 +21,27 @@ namespace prefixgenerate
         private void button1_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog fbd = new FolderBrowserDialog();
-                if (fbd.ShowDialog() == DialogResult.OK)
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string[] subdirs = Directory.GetDirectories(fbd.SelectedPath);
+                    
+                foreach (string x in subdirs)
                 {
-                    string[] subdirs = Directory.GetDirectories(fbd.SelectedPath);
-                    
-                    foreach (string x in subdirs)
+                    string path = x.Remove(0, 3);
+                    string path_create = x + ".txt";
+                    if (File.Exists(path_create))
                     {
-                        string path = x.Remove(0, 3);
-                        string path_create = x + ".txt";
-                        if (File.Exists(path_create))
-                            {
-                                File.Delete(path_create);
-                            }
-                        using (StreamWriter sw = File.CreateText(path_create))
-                        {
-                            sw.WriteLine("Prefix=" + path + @"\");
-                            sw.WriteLine("product=Arma 3");
-                            sw.WriteLine("version=149197");
-                        }
+                        File.Delete(path_create);
+                    }
 
-
-                }
-                    
-                }
-
-}
+                    using (StreamWriter sw = File.CreateText(path_create))
+                    {
+                        sw.WriteLine("Prefix=" + path + @"\");
+                        sw.WriteLine("product=Arma 3");
+                        sw.WriteLine("version=149197");
+                    }
+                }    
+            }
+        }
     }
 }
